6d3050020d536bccdf06e0ad25fc6924e400d38e
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2024-06-05 16:07:08 -0300

    Starting REST APIs

--- sandbox/sandbox/src/main/java/com/gustavofdasilva/sandbox/SandboxApplication.java
+++ sandbox/sandbox/src/main/java/com/gustavofdasilva/sandbox/SandboxApplication.java
@@ -8,13 +8,6 @@ public class SandboxApplication {
 
 	public static void main(String[] args) {
 		var ctx = SpringApplication.run(SandboxApplication.class, args);
-
-		FirstService firstService = ctx.getBean(FirstService.class);
 		
-		
-		System.out.println(firstService.tellAStory());
-		System.out.println(firstService.getJavaVersion());
-		System.out.println(firstService.getProjectName());
-		System.out.println(firstService.getCustomProperty());
 	}
 }
