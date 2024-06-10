8f8a2d1f29e97918b2e30f9c6061c6bf4f4afa7b
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2024-06-10 17:20:46 -0300

    Starting PostgreSQL Connection

--- sandbox/sandbox/src/main/java/com/gustavofdasilva/sandbox/FirstController.java
+++ sandbox/sandbox/src/main/java/com/gustavofdasilva/sandbox/FirstController.java
@@ -1,81 +1,12 @@
 package com.gustavofdasilva.sandbox;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
     @GetMapping("/helloworld")
     public String sayHello() {
+        Student student = new Student();
         return "HELLO! from my first controller!";
-    }
-
-    @PostMapping("/sendOrder")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String sendOrder(
-        @RequestBody Order order
-    ) {
-        order.setCustomerName(order.getCustomerName().replace("A", "O"));
-        return order.toString();
-    }
-
-    @PostMapping("/sendOrder-record")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String sendOrderRecord(
-        @RequestBody OrderRecord order
-    ) {
-        return order.toString();
-    }
-
-    @GetMapping("/hello/{page}")
-    public String pathVar(
-        @PathVariable String page,
-        @RequestParam("id") String id
-    ) {
-        
-        return "PATHVALUE: "+ page + "; PARAMS: "+id;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @GetMapping("/calculator/{operation}")
-    public int calculate(
-        @PathVariable String operation,
-        @RequestParam("n1") int num1,
-        @RequestParam("n2") int num2
-    ) {
-        switch(operation) {
-            case "add":
-                return num1 + num2;
-        
-            case "subtract":
-                return num1 - num2;
-            
-            case "multiply":
-                return num1 * num2;
-
-            case "divide":
-                return num1 / num2;
-
-            default:
-                return 0;
-        }
     }
 }
---
+++ sandbox/sandbox/src/main/java/com/gustavofdasilva/sandbox/Student.java
@@ -0,0 +1,71 @@
+package com.gustavofdasilva.sandbox;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="T_STUDENT")
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email", unique=true)
+    private String email;
+
+    @Column(name="age", length=100)
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    
+}
